e60e462f9b804a1b4d353082df5d30a2c269e702
Berrybunny015 <berrybunny015@gmail.com>
2021-10-11 19:50:23 -0400

    fixed ex44 test code

--- src/test/java/ex44/AppTest.java
+++ src/test/java/ex44/AppTest.java
@@ -9,7 +9,7 @@ public class AppTest {
     @Test
     public void main() {
         //to check that the exercise 44 json input file exists
-        File ex44InputFile = new File("src\\main\\java\\ex41\\exercise44_input.json");
+        File ex44InputFile = new File("src\\main\\java\\ex44\\exercise44_input.json");
         assertTrue(ex44InputFile.exists()); //assert true is the file does exist
 
         long jsonFileLength = ex44InputFile.length(); //declare and initialize "jsonFileLength" with the long datatype to find length of json input file
